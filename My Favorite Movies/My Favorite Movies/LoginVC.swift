1dac92ca75395861385dbbc208a1f536c2eec487
Billy <mr.bill.ly@gmail.com>
2017-01-20 14:54:48 -0800

    added projects, create views

---
+++ My Favorite Movies/My Favorite Movies/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  My Favorite Movies
+//
+//  Created by Billy on 1/20/17.
+//  Copyright © 2017 Billy. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
