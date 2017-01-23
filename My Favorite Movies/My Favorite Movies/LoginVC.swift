999e34ab925a10aee12f61853461df450e488714
Billy <mr.bill.ly@gmail.com>
2017-01-22 21:11:46 -0800

    apps working

--- My Favorite Movies/My Favorite Movies/LoginVC.swift
+++ My Favorite Movies/My Favorite Movies/LoginVC.swift
@@ -51,19 +51,7 @@ class LoginVC: UIViewController {
             debugTextLabel.text = "Username or Password Empty."
         } else {
             setUIEnabled(false)
-            
-            /*
-             Steps for Authentication...
-             https://www.themoviedb.org/documentation/api/sessions
-             
-             Step 1: Create a request token
-             Step 2: Ask the user for permission via the API ("login")
-             Step 3: Create a session ID
-             
-             Extra Steps...
-             Step 4: Get the user id ;)
-             Step 5: Go to the next view!
-             */
+        
             getRequestToken()
         }
     }
